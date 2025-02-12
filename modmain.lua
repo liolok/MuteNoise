d51c2d1ec04f14a0ce1105cf183ec287c91b42d8
liolok <i@liolok.com>
2025-02-12 14:45:09 +0800

    add wx78_scanner

--- modmain.lua
+++ modmain.lua
@@ -89,6 +89,7 @@ Hook('Worm Hole', 'dontstarve/common/teleportworm/idle')
 
 Hook('Abigail', 'dontstarve/characters/wendy/abigail/howl')
 Hook('Battle Saddle Music', 'dontstarve/music/music_wigfrid_valkyrie')
+Hook('Bio Scanalyzer Chirp', 'WX_rework/scanner/ping')
 Hook('Charged Elding Spear', 'meta3/wigfrid/spear_wathrithr_lightning_charged')
 Hook('Chorusbox Circuit', 'WX_rework/module/musicmodule_lp')
 Hook('Lucy the Axe', 'dontstarve/characters/woodie/lucytalk_LP')
